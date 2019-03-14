3a8ade64e12ccdb362f9fe1d108a10ebbf1412b2
KR4-T0S <richie@richiegfx.net>
2019-03-13 19:43:18 -0700

    Probe C now properly sends to message queue. Datahub reads data from Probe C. Kill Patch not yet working.

--- MessageQueue/DataHub.cpp
+++ MessageQueue/DataHub.cpp
@@ -6,4 +6,36 @@
 //  Copyright © 2019 StevenOnSoftware. All rights reserved.
 //
 
-#include <stdio.h>
+#include <sys/types.h>
+#include <sys/ipc.h>
+#include <sys/msg.h>
+#include <cstring>
+#include <iostream>
+#include <unistd.h>
+#include <sys/wait.h>
+#include <cstdlib>
+using namespace std;
+
+int main() {
+    // create my msgQ with key value from ftok()
+	int qid = msgget(ftok(".",'u'), IPC_EXCL|IPC_CREAT|0600);
+
+    // declare my message buffer
+	struct buf {
+		long mtype; // required
+		char greeting[50]; // mesg content
+	};
+
+	buf msg;
+	int size = sizeof(msg)-sizeof(long);
+
+	// Read message (Probe C)
+	msgrcv(qid, (struct msgbuf *)&msg, size, 251, 0); // read message from probe c (251 aka Rho)
+    cout << getpid() << " (Data Hub): Found reading" << endl;
+    cout << msg.greeting << endl;
+
+	// Delete msgqueue
+	msgctl (qid, IPC_RMID, NULL);
+
+    exit(0);
+}
--- MessageQueue/ProbeC.cpp
+++ MessageQueue/ProbeC.cpp
@@ -12,14 +12,13 @@
 #include <sys/wait.h>
 #include <cstdlib>
 #include <string>
+#include "kill_patch.h"
 using namespace std;
 
 const int MAGIC_SEED_RHO = 251;
 
 int main() {
-    srand(time(0));
-    // generates system wide key for the queue
-    int qid = msgget(ftok(".",'u'), 0);
+    srand(time(0)); // Seed for better random values
 
     // Message buffer struct
     struct buf {
@@ -27,11 +26,20 @@ int main() {
         char greeting[50];
     };
 
+    // generates system wide key for the queue
+    int qid = msgget(ftok(".",'u'), 0);
+
     buf msg;
 	int size = sizeof(msg)-sizeof(long);
+
+    // Call kill patch function
+    msg.mtype = 521; // Set to mtype for exit
+    string exitmsg = "Probe C is closing.";
+    strcpy(msg.greeting, exitmsg.c_str() );
+    kill_patch(qid, (struct msgbuf*)&msg, size, msg.mtype);
     
 
-    cout << "/* --------------- Probe C --------------- */" << endl;
+    cout << "Probe C: Finding valid reading" << endl;
     bool isValid = false; // Flag to stop validating 
     do {
         // Valid Reading
@@ -39,19 +47,18 @@ int main() {
         int reading = randValue % MAGIC_SEED_RHO;
         if (reading == 0) { // random integer is divisible by seed.
             isValid = true; // Toggle flag
-
-            string sendingMsg = "";
-            char tempmsg[50];
-            strcpy(tempmsg, "Valid Reading Found");
-            cout << tempmsg << ": "<< randValue << endl;
+            string sendingMsg = ""; // Message sent to queue
 
             // send to DataHub
-            msg.mtype = 117;
-            sendingMsg = "251:" + to_string(randValue);
-            strcpy(msg.greeting, sendingMsg.c_str() );
-            msgsnd(qid, (struct msgbuf*)&msg, size, 0); // send message to queue
+            msg.mtype = 251; // mtype set to seed, why not?
+            sendingMsg = to_string(getpid()) + " (Probe C): " + to_string(randValue); // set message to send as PID of probe + valid reading
+            strcpy(msg.greeting, sendingMsg.c_str() ); // Copy message string to char array
+            msgsnd(qid, (struct msgbuf*)&msg, size, 0); // Send message to queue
         }
     } while (!isValid);
+
+    // Delete msgqueue when testing probe C only
+	msgctl (qid, IPC_RMID, NULL);
 
     exit(0);
 }
