f64216cb56088a0c3a95bd6c748adfd5820c7716
josefranco7 <josefrancoooo7@gmail.com>
2019-03-19 15:14:37 -0700

    Update ProbeB for DataHub

--- MessageQueue/ProbeB.cpp
+++ MessageQueue/ProbeB.cpp
@@ -1,11 +1,11 @@
-//
-//  ProbeB.cpp
-//  MessageQueue
-//
-//  Created by  on 2/21/19.
-//  Copyright © 2019 StevenOnSoftware. All rights reserved.
-//
 
+//
+// ProbeB.cpp
+// MessageQueue
+//
+// Created by on 2/21/19.
+// Copyright © 2019 StevenOnSoftware. All rights reserved.
+//
 #include <stdio.h>
 #include <sys/types.h>
 #include <sys/ipc.h>
@@ -17,70 +17,54 @@
 #include <cstdlib>
 #include <limits>
 #include <string>
-#include <stdlib.h>     /* srand, rand */
-#include <time.h>       /* time */
+#include <stdlib.h> /* srand, rand */
+#include <time.h> /* time */
+#include "force_patch.h"
 
 using namespace std;
+
+void force_patch(int, struct msgbuf *, int, long);
 
 const int PROBE_B_SEED = 257;
 const int MAX_INT_LIMIT = std::numeric_limits<int>::max();
 const int DATA_HUB_LIMIT = 10000;
 
 int main() {
+	srand(time(0));
+	
+	bool isExecuting = true;
+	string hubMessage = "";
+	// generates system wide key for the queue
+	int qid = msgget(ftok(".",'u'), 0);
+	cout << "qid is " << qid << endl;
+	// declare my message buffer
+	struct buf {
+	long mtype; // required
+	char greeting[50]; // mesg content
+	};
+	
+	buf msg;
+	int size = sizeof(msg)-sizeof(long);
 
-    srand(time(0));
-    
-    bool isExecuting = true;
-    string hubMessage = "";
-    
-    // generates system wide key for the queue
-    int qid = msgget(ftok(".",'u'), 0);
-    cout << "qid is " << qid << endl;
-    
-    // declare my message buffer
-    struct buf {
-        long mtype; // required
-        char greeting[50]; // mesg content
-    };
-    buf msg;
-    int size = sizeof(msg)-sizeof(long);
-    
-    cout << size << endl;
-    cout << "Magic ProbeB seed: " << PROBE_B_SEED << endl; 
-    cout << "Probe B begins here: " << endl;
-    int count = 0;//messages sent to the queue
-   
-    while (isExecuting) {
+	cout << "Magic ProbeB seed: " << PROBE_B_SEED << endl;
+	cout << "Probe B begins here: " << endl;
 
-        // generate a valid random number
-        int random = MAX_INT_LIMIT;
-        
-        if(random % PROBE_B_SEED != 0){
-            random = rand() % MAX_INT_LIMIT;
-            count++;
-            cout << "The random value is: " << random << endl;
-            
-            //Send message to the hub
-            msg.mtype = PROBE_B_SEED;
-            hubMessage = to_string(getpid()) + " (Probe B): " + to_string(random);
-            strcpy(msg.greeting, hubMessage.c_str() );
-            msgsnd(qid, (struct msgbuf *)&msg, size, 0); // send message to queue
-            
-            if (count > DATA_HUB_LIMIT) {
-                cout << "Probe B will terminate since the Data Hub has recieved 10,000 messages." << endl;
-                isExecuting = false;
-                msg.mtype = PROBE_B_SEED;
-                hubMessage = "Probe B Exit";
-                strcpy(msg.greeting, hubMessage.c_str() );
-                msgsnd(qid, (struct msgbuf *)&msg, size, 0); // send message to queue
-                
-                isExecuting = false;
-                continue;
-            }
-        }
-        
-        
-    }
+	hubMessage = to_string(getpid()) + "ProbeB";
+	strcpy(msg.greeting, hubMessage.c_str() );
+	msgsnd(qid, (struct msgbuf *)&msg, size, 0); // send message to queue
 
-    return 0;
+	while (isExecuting) {
+		// generate a valid random number
+		int random=MAX_INT_LIMIT;
+		if(random % PROBE_B_SEED != 0){
+			random = rand() % MAX_INT_LIMIT;
+			cout << "The random value is: " << random << endl;
+		}
+		// send to DataHub
+		msg.mtype = MAX_INT_LIMIT;
+		hubMessage = to_string(getpid()) + "ProbeB" + to_string(random);
+		strcpy(msg.greeting, hubMessage.c_str() );
+		msgsnd(qid, (struct msgbuf *)&msg, size, 0); // send message to queue
+	}
+	return 0;
 }
